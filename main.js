4c91e22d583bd4a0241c1610e0efb4fbbf181fcb
unknown <krishaang_1808@hotmail.com>
2023-12-24 04:16:17 +0530

    added verman and fixed client as host bug

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const fs = require("fs");
 const mongoose = require("mongoose");
 const handlebars = require("express-handlebars").engine;
 const path = require("path");
+const bodyParser = require("body-parser");
 
 app.engine("handlebars", handlebars({
         layoutsDir: __dirname + "/views/layouts/",
@@ -15,16 +17,21 @@ app.engine("handlebars", handlebars({
     }));
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.DB_URL);
 
 const Host = mongoose.model("Hosts", {
-    hostname: { required: true, type: String },
+    hostname: { required: true, type: String, unique: true },
     dateCreated: { type: Date, default: Date.now },
     status: { type: String, default: "active" },
     lastActive: { type: Date, default: Date.now },
 });
+
+const Version = mongoose.model("Versions", {
+    version: { required: true, type: String, unique: true },
+    url: { required: true, type: String, unique: true }
+})
 
 app.get("/", async (req,res) => {
     const opts = {
@@ -33,6 +40,36 @@ app.get("/", async (req,res) => {
     };
     console.log("SERVING WITH OPTIONS,\n", opts);
     res.render("index", opts);
+});
+
+app.get("/addHostVersion", (req, res) => {
+    res.render("addHostVersion", {
+        title: "New Host Version"
+    });
+});
+
+app.get("/verman", async (req, res) => {
+    let _verinfo = await Version.find().lean();
+    let verinfo = [];
+    _verinfo.forEach(ver => {
+        verinfo.push({ version: ver.version, url: ver.url });
+    });
+    res.json(verinfo).status(200);
+});
+
+app.post("/verman", async (req, res) => {
+    console.log(req.body)
+    if (!req.body.apikey) {res.send("The api key is required.").status(400); return false};
+    if (req.body.apikey !== "kasdev") {res.send("Incorrect api key.").status(401); return false};
+    if (!req.body.version || !req.body.url) {res.send("Both `version` and `url` are required.").status(400); return false};
+
+    if (await Version.findOne({$or: [{ version: req.body.version }, { url: req.body.url }]})) {res.send("Version or URL already exist").status(409); return false};
+
+    let ver = await Version.create({ version: req.body.version, url: req.body.url });
+
+    res.send("Success.").status(200);
+
+    return true;
 });
 
 app.get("/raw", (req,res) => {
@@ -49,19 +86,21 @@ io.on("connection", (socket) => {
 
         let clientDBInstance = undefined;
 
-        const existingHost = await Host.findOne({ hostname: client_info.client_name });
-
-        if (existingHost) {
-            existingHost.lastActive = Date.now();
-            existingHost.status = "active";
-            await existingHost.save();
-            clientDBInstance = existingHost;
-        } else {
-            clientDBInstance = await Host.create({
-                hostname: client_info.client_name,
-                lastActive: Date.now(),
-                status: "active"
-            });
+        if (client_info.client_type === "HOST") {
+            const existingHost = await Host.findOne({ hostname: client_info.client_name });
+            
+            if (existingHost) {
+                existingHost.lastActive = Date.now();
+                existingHost.status = "active";
+                await existingHost.save();
+                clientDBInstance = existingHost;
+            } else {
+                clientDBInstance = await Host.create({
+                    hostname: client_info.client_name,
+                    lastActive: Date.now(),
+                    status: "active"
+                });
+            }
         }
 
         socket.on("term.onData", (data) => {
